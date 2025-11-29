e841ba3b6b0d8a2006b90554e1f229d23b158e20
shchanad <s4dned@gmail.com>
2025-11-29 23:33:16 +0100

    Add animation and sprites

--- KnifePath.gd
+++ KnifePath.gd
@@ -1,5 +1,36 @@
 class_name KnifePath
 extends PathFollow2D
+var isAnimated: bool = false
+var start_offset := 0.0
+var end_offset := 150.0   # опускание вниз
+
+
+func animate():
+	if isAnimated: return
+	isAnimated = true
+
+	var tween = create_tween()
+
+	# Опустить вниз на 100
+	tween.tween_property(self, "v_offset", end_offset, 0.05)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	# Вернуть назад
+	tween.tween_property(self, "v_offset", start_offset, 0.07)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	tween.finished.connect(func():
+		isAnimated = false
+	)
+	
+
+func _process(delta: float) -> void:
+	# обычное движение по Path
+	#progress_ratio += delta * 0.2
+
+	if Input.is_action_just_pressed("ui_accept"):
+		animate()
 
 func reposition(t: float) -> void:
 	progress_ratio = t 
+	
